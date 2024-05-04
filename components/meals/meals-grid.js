eadbe8952f52b576c4d0ceb9450343d1d0d5dfbb
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-04 18:11:56 +0300

    Implemented a meals list with dummy data and a component for individual meals along with their respective styles.

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -4,7 +4,7 @@ import styles from './meals-grid.module.css';
 export default function MealsGrid({ meals }) {
 
     return (
-        <ul>
+        <ul className={styles.meals}>
             {meals.map(meal => (
                 <li key={meal.id}>
                     <MealItem {...meal}/>

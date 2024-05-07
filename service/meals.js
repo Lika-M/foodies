0380447e7ba99c3b55525983e6a497cce1a14a0b
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-05-07 07:41:04 +0300

    Handling errors related to the server and data retrieval from the database.

--- service/meals.js
+++ service/meals.js
@@ -3,8 +3,9 @@ import sql from 'better-sqlite3';
 const db = sql('meals.db');
 
 export async function getAllMeals() {
-    // throw new Error('Something happens');
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // throw new Error('Failed to fetch data. Please try again later.');
 
+    // delay fetching data
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     return db.prepare('SELECT * FROM meals').all();
 }
